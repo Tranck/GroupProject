672f5fd593658006e55c8a783264a44ba6fb5152
new game <naamalivnee@gmail.com>
2026-09-08 14:59:17 +0300

    prepare merge

--- Screen.py
+++ Screen.py
@@ -7,7 +7,6 @@ screen = pygame.display.set_mode(
         (consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
 screen.fill(consts.GREEN)
 
-
 objects = {} #loading all the images into here
 
 def create_soldier():
